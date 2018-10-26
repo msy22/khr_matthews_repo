4e85ca1e69dac47b372bd250f19943c74a70a492
M@t <msy22@uclive.ac.nz>
2018-10-26 14:07:59 +1300

    Initial commit

---
+++ src/tool_actuator/src/tool_actuator.cpp
@@ -0,0 +1,71 @@
+//// Includes_____________________________________________________________________
+//#include <ros/ros.h>
+//#include <std_msgs/Bool.h>
+//#include <move_base_msgs/MoveBaseAction.h>
+//#include <actionlib/client/simple_action_client.h>
+//#include <nav_msgs/Odometry.h>
+//#include <sensor_msgs/Imu.h>
+//#include <geometry_msgs/Twist.h>
+//#include <geometry_msgs/Transform.h>
+//#include <geometry_msgs/TransformStamped.h>
+
+//#include <vector>
+//#include <string>                             // stod
+//#include <cstdio>
+//#include <cstring>
+
+//// Setup namespaces_____________________________________________________________
+//using namespace std;
+
+
+//// Global Constants_____________________________________________________________
+//const int _port_number = 2001;
+
+
+//// Code_________________________________________________________________________
+
+
+//// This callback works like an interrupt, and will be executed every time the
+//// /tool_actuated rostopic is updated
+//void CallbackToolActuated (std_msgs::Bool tool_msg)
+//{
+//  /* TODO
+//   * - Save/send the tool data to TA
+//   * - Set the tool_actuated rostopic to false to let node 2 (robot_navigator)
+//   *   know that it can proceed with the next waypoint
+//   *
+//   *
+//   */
+//}
+
+
+//// This callback works like an interrupt, and will be executed every time the
+//// /job_completed rostopic is updated
+//void CallbackJobCompleted (std_msgs::Bool job_msg)
+//{
+//  /* TODO
+//   * Send the "job done" message to TA
+//   */
+//}
+
+
+//int main(int argc, char **argv)
+//{
+//  // Setup ROS node, including publishers and subscribers
+//  ros::init(argc, argv, "tool_actuator");
+//  ros::NodeHandle nh("~");
+//  ros::Publisher tool_publisher   = nh.advertise<std_msgs::Bool>("tool_actuated", 10);
+//  ros::Subscriber tool_subscriber = nh.subscribe("tool_actuated", 10, CallbackToolActuated);
+//  ros::Subscriber job_subscriber  = nh.subscribe("job_completed", 10, CallbackJobCompleted);
+
+//  /* TODO
+//   * Establish a connection to TA on port 2001
+//   *
+//   */
+
+//  // Sit here waiting, allowing the callbacks to run and only ending when
+//  // the node is shut down with "Ctrl+C"
+//  ros::spin();
+
+//  return 0;
+//}
---
+++ src/trimble_access_listener/src/trimble_access_listener.cpp
@@ -0,0 +1,81 @@
+//// Includes____________________________________________________________________________
+//#include "ros/ros.h"
+//#include "std_msgs/String.h"
+//#include "nav_msgs/Odometry.h"
+//#include "tf/tfMessage.h"
+//#include "sensor_msgs/Imu.h"
+//#include <tf/transform_broadcaster.h>
+//#include "geometry_msgs/TransformStamped.h"
+//#include <sstream>
+
+//// Globals____________________________________________________________________________
+//string _default_imu_topic = "imu/data";   // this will be /microstrain_imu/data/enu on jackal
+//sensor_msgs::Imu _latest_imu_msg;
+
+
+//// Code____________________________________________________________________________
+
+
+
+////void CallbackImu (const sensor_msgs::Imu imu_msg)
+////{
+////  // Just update the global storage for the IMU message
+////  _latest_imu_msg = imu_msg;
+////}
+
+
+
+//void ParseTrimbleAccessMessage (string input_ta_msg,
+//                                nav_msgs::Odometry& output_odom)
+//{
+
+//}
+
+
+
+//void CalculateHeadingFromPosition ()
+//{
+
+//}
+
+
+
+//int main(int argc, char **argv)
+//{
+//  // Set up ROS node, including publishers and subscribers
+//  ros::init(argc, argv, "trimble_access_listener");
+//  ros::NodeHandle nh("~");
+//  ros::Rate loop_rate(10);      // rate for the main loop to cycle through in Hz
+//  ros::Publisher odom_publisher = nh.advertise<nav_msgs::Odometry>("odometry/measured", 1000);
+//  //ros::Subscriber sub = nh.subscribe("imu/data", 10, CallbackImu);
+  
+//  string latest_ta_position;
+//  nav_msg::Odometry latest_odom_msg;
+
+//  // Enter into a loop, consistently polling TA for positions, only exits
+//  // when "Ctrl + C" is pressed
+//  while (ros::ok())
+//  {
+//    /* TODO
+//     * Poll or wait for TA to return the next position estimate as a string
+//     *
+//     */
+
+//    // Parse the string
+//    ParseTrimbleAccessMessage(latest_ta_position, latest_odom_msg);
+
+//    // Publish the odometry
+//    odom_publisher.publish(latest_odom_msg);
+
+//    // These functions make sure this while loop runs at 10 Hz, and allows
+//    // callbacks to run
+//    ros::spinOnce();
+//    loop_rate.sleep();
+//  }
+
+//  // Sit here waiting, allowing the callbacks to run and only ending when
+//  // the node is shut down with "Ctrl+C"
+//  ros::spin();
+
+//  return 0;
+//}
