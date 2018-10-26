4da3b7c8a963c6644de760271dacd28f89e19244
M@t <msy22@uclive.ac.nz>
2018-10-26 15:54:18 +1300

    minimum working example for trimble_access_listener

--- src/knee_high_robotics/src/trimble_access_listener.cpp
+++ src/knee_high_robotics/src/trimble_access_listener.cpp
@@ -12,20 +12,11 @@
 using namespace std;
 
 // Globals____________________________________________________________________________
-string _default_imu_topic = "imu/data";   // this will be /microstrain_imu/data/enu on jackal
-sensor_msgs::Imu _latest_imu_msg;
+const int _port_num = 2000;
+const int _loop_rate = 1;       // rate for the main loop to cycle through in Hz
 
 
 // Code____________________________________________________________________________
-
-
-
-void CallbackImu (const sensor_msgs::Imu imu_msg)
-{
-  // Just update the global storage for the IMU message
-  _latest_imu_msg = imu_msg;
-}
-
 
 
 //void ParseTrimbleAccessMessage (string input_ta_msg,
@@ -36,45 +27,37 @@ void CallbackImu (const sensor_msgs::Imu imu_msg)
 
 
 
-//void CalculateHeadingFromPosition ()
-//{
-
-//}
-
-
 
 int main(int argc, char **argv)
 {
   // Set up ROS node, including publishers and subscribers
   ros::init(argc, argv, "trimble_access_listener");
-  ros::NodeHandle nh("~");
-  ros::Rate loop_rate(10);      // rate for the main loop to cycle through in Hz
+  ROS_INFO("Setting up trimble_access_listener");
+  ros::NodeHandle nh;
+  ros::Rate loop_rate(_loop_rate);
   ros::Publisher odom_publisher = nh.advertise<nav_msgs::Odometry>("odometry/measured", 1000);
-  //ros::Subscriber sub = nh.subscribe("imu/data", 10, CallbackImu);
   
-//  string latest_ta_position;
-//  nav_msg::Odometry latest_odom_msg;
+  string latest_ta_position;
+  nav_msgs::Odometry latest_odom_msg;
 
-//  // Enter into a loop, consistently polling TA for positions, only exits
-//  // when "Ctrl + C" is pressed
-//  while (ros::ok())
-//  {
-//    /* TODO
-//     * Poll or wait for TA to return the next position estimate as a string
-//     *
-//     */
+  // Enter into a loop, consistently polling TA for positions, only exits
+  // when "Ctrl + C" is pressed
+  while (ros::ok())
+  {
+    /* TODO
+     * Poll or wait for TA to return the next position estimate as a string
+     *
+     */
 
-//    // Parse the string
-//    ParseTrimbleAccessMessage(latest_ta_position, latest_odom_msg);
+    // Publish the odometry
+    ROS_INFO("Spinning at 1Hz, publishing a blank odometry msgs");
+    odom_publisher.publish(latest_odom_msg);
 
-//    // Publish the odometry
-//    odom_publisher.publish(latest_odom_msg);
-
-//    // These functions make sure this while loop runs at 10 Hz, and allows
-//    // callbacks to run
-//    ros::spinOnce();
-//    loop_rate.sleep();
-//  }
+    // These functions make sure this while loop runs at 10 Hz, and allows
+    // callbacks to run
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
 
   // Sit here waiting, allowing the callbacks to run and only ending when
   // the node is shut down with "Ctrl+C"
